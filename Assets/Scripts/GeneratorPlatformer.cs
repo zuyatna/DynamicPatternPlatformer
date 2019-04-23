855f803dfc445bc2395e39254add87282ff48eb4
suyatna <antasuy@gmail.com>
2019-04-23 13:57:08 +0700

    Adding first file

---
+++ Assets/Scripts/GeneratorPlatformer.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+
+public class GeneratorPlatformer : MonoBehaviour {
+
+    public static GeneratorPlatformer Instance;
+
+    public float DistanceBetween;
+    public Transform GenerationPoint;
+    public ObjectPooler[] theObjectPools;
+    
+    [HideInInspector] public int PlatformSelector;
+
+    void Awake()
+    {
+        Instance = this;
+    }
+
+    void FixedUpdate()
+    {
+        if (transform.position.y < GenerationPoint.position.y)
+        {
+            PositionTransform();
+        }
+    }
+
+    public void PositionTransform() {
+    
+        transform.position = new Vector3(transform.position.x, transform.position.y + DistanceBetween, 0);
+
+        PlatformSelector = Random.Range(0, theObjectPools.Length);
+
+        // Spawn pooling
+        GameObject newPlatform = theObjectPools[PlatformSelector].GetPooledObject();
+
+        newPlatform.transform.position = transform.position;
+        newPlatform.transform.rotation = transform.rotation;
+        newPlatform.SetActive(true);
+
+        transform.position = new Vector3(transform.position.x, transform.position.y, 0);
+    }
+}
---
+++ Assets/Scripts/ObjectPooler.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectPooler : MonoBehaviour
+{
+    public static ObjectPooler SharedInstance;
+
+    public List<GameObject> pooledObjects;
+    public GameObject objectToPool;
+    public int amountToPool;
+
+    private void Awake()
+    {
+        SharedInstance = this;
+    }
+
+    private void Start()
+    {
+        pooledObjects = new List<GameObject>();
+        for (int i = 0; i < amountToPool; i++)
+        {
+            GameObject obj = (GameObject)Instantiate(objectToPool);
+            obj.SetActive(false);
+            pooledObjects.Add(obj);
+        }
+    }
+
+    public GameObject GetPooledObject()
+    {
+        for (int i = 0; i < pooledObjects.Count; i++)
+        {
+            if (!pooledObjects[i].activeInHierarchy)
+            {
+                return pooledObjects[i];
+            }
+        }
+
+        return null;
+    }
+}
