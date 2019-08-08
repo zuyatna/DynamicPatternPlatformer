d9891a3065fd6fea97a04daec2e7a1542e9bdbbb
Cathean <ivanshadow19@gmail.com>
2019-08-08 15:23:15 +0700

    Add broken platform animations

---
+++ Assets/Scripts/PlatformShatter/Shatter.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shatter : MonoBehaviour
+{
+    public GameObject slicedPlatform;
+    public float shakeAmount = 0.025f;
+    public float speed = 5f;
+
+    private bool isShake = false;
+
+    void Start()
+    {
+        slicedPlatform.SetActive(false);
+    }
+
+    void OnMouseDown()
+    {
+        if(!isShake)
+            isShake = true;
+        else
+        {
+            isShake = false;
+            gameObject.SetActive(false);
+            slicedPlatform.SetActive(true);
+        }
+    }
+
+    private void Update()
+    {
+        if(isShake)
+            shakeAnim();
+    }
+
+    void shakeAnim()
+    {
+        Vector3 tempPos = transform.position;
+        Vector3 startingPos = transform.position;
+
+        tempPos.x = startingPos.x + Mathf.Sin(Time.time * speed) * shakeAmount;
+        transform.position = tempPos;
+        //Debug.Log(transform.position.x + ", " + tempPos.x);
+    }
+}
